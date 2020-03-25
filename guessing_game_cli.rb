6382cd6c65c5e111601a2f3c58fc6705f5d84cf1
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-25 16:45:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -18,16 +18,3 @@ def run_guessing_game
     else
 
 end
-
-def run_guessing_game
-  random = rand(6) + 1
-  puts "Guess a number between 1 and 6"
-  user_input = gets.chomp
-  if user_input == random.to_s
-    puts "You guessed the correct number!"
-  elsif user_input == "exit"
-    puts "Goodbye!"
-  else
-    puts "Sorry! The computer guessed #{random}."
-  end
-end
